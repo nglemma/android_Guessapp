57bfef10657b15335e0e80d7584fd932f0287125
nglemma <ngeleemmanuel@yahoo.com>
2020-03-11 17:24:26 +0100

    guesswork

--- app/src/main/java/com/example/guessap/MainActivity.java
+++ app/src/main/java/com/example/guessap/MainActivity.java
@@ -3,11 +3,41 @@ package com.example.guessap;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    public void jaccuzi(View view)
+    {
+        EditText entervalue= findViewById(R.id.entervalue);
+        final int random = new Random().nextInt(10);
+        int userguess= Integer.parseInt(entervalue.getText().toString());
+        if (userguess > random)
+        {
+            Toast.makeText(this,"Go Lower",Toast.LENGTH_SHORT).show();
+        }
+        else if(userguess<random)
+        {
+            Toast.makeText(this,"Go Higher",Toast.LENGTH_SHORT).show();
+        }
+        else if(userguess==random)
+        {
+            Toast.makeText(this,"Correct Guess",Toast.LENGTH_SHORT).show();
+        }
+        else
+            {
+                Toast.makeText(this,"Invalid Entry",Toast.LENGTH_SHORT).show();
+            }
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
